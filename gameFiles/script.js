b086408dbe82ef53aedb5d5c8f7b9554a89ae6aa
Simon Burkhardt <sburkhardt@stud.hs-bremen.de>
2022-08-02 17:52:55 +0200

    added collision with obstacles

--- gameFiles/script.js
+++ gameFiles/script.js
@@ -28,6 +28,8 @@ let gameOver = false;
 let pauseText;
 let gamePaused = false;
 let backgroundMusic;
+let score = 0;
+let scoreText;
 
 const game = new Phaser.Game(config);
 
@@ -36,8 +38,7 @@ function preload ()
     // loading ground
     // v1
     this.load.spritesheet('block', '../assets/block.png', {frameWidth: 50, frameHeight: 50});
-    // v2
-    this.load.spritesheet('block', '../assets/blockv2.png', {frameWidth: 50, frameHeight: 50});
+    // this.load.spritesheet('blockv2', '../assets/blockv2.png', {frameWidth: 50, frameHeight: 50}); // v2
 
 
     // loading obstacles
@@ -68,17 +69,21 @@ function create ()
 
     // adding player character to the game and physics
     player = this.physics.add.sprite(400, 350, 'block');
+    // player = this.physics.add.sprite(400, 350, 'blockv2'); //v2
 
     // player gravity
     player.body.setGravityY(300);
     // player collider with world bounds and ground
     // player.setCollideWorldBounds(true);
+
+    // collider
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, obstacles, die, null, this);
 
     // player block animations
     this.anims.create({
         key: 'jump',
-        frames: this.anims.generateFrameNumbers('block', {start: 0, end: 4}),
+        frames: this.anims.generateFrameNumbers('block', {start: 0, end: 9}),
         frameRate: 5,
         repeat: 0
     });
@@ -86,8 +91,9 @@ function create ()
     // input
     cursors = this.input.keyboard.createCursorKeys();
 
-    //  pauseButton
+    //  pauseButton + score
     pauseText = this.add.text(680, 16, 'Pause', {fontSize: '2rem', fill: '#fff'});
+    scoreText = this.add.text(16, 16, 'Versuch 0', {fontSize: '2rem', fill: '#fff'});
 
     // camera following character
     this.cameras.main.startFollow(player, false, 1, 0, -175  , 150);
@@ -106,8 +112,10 @@ function update ()
 
     // set player movement
     player.setVelocityX(200);
-    // sets position from pauseText relative to player Character
+    console.log(player.x);
+    // sets position from pauseButton and score relative to player Character
     pauseText.x = player.x + 450;
+    scoreText.x = player.x - 200;
 
     // make camera follow player
     // lerpY: 0; doesn't follow jumps // offsetY: 100; fixes position
@@ -119,9 +127,9 @@ function update ()
     }*/
 
     // TODO fix esc pause
-    if(esc.isDown) {
+    /*if(esc.isDown) {
         pauseGame();
-    }
+    }*/
 }
 
 function pauseGame() {
@@ -141,4 +149,10 @@ function pauseGame() {
         // pauses scene
         game.scene.pause('default');
     }
+}
+
+function die() {
+    score++;
+    scoreText.setText('Versuch ' + score);
+    window.alert("game over");
 }
